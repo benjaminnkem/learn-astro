eb7000cf25102d7d5002a5b0cdcc75091b8a989f
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-04-13 13:51:52 +0100

    update: dontknowyet section

--- src/components/UI/Home/Tsx/DontKnowYet.tsx
+++ src/components/UI/Home/Tsx/DontKnowYet.tsx
@@ -3,6 +3,7 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useEffect, useRef } from "react";
 import MaterialSymbolsImageRounded from "../../../Common/Icons/image-rounded.icon";
+import classNames from "classnames";
 
 const DontKnowYet = () => {
   const ref = useRef<HTMLElement>(null);
@@ -36,7 +37,11 @@ const DontKnowYet = () => {
 
   return (
     <section
-      className="min-h-screen pt-40 pb-64 2xl:h-auto 2xl:py-40 relative before:absolute before:top-0 before:left-0 before:w-full before:h-[10rem] before:content-[''] before:bg-gradient-to-b before:from-dark"
+      className={classNames([
+        "min-h-screen pt-40 pb-64 2xl:h-auto 2xl:py-40 relative",
+        "before:absolute before:top-0 before:left-0 before:w-full before:h-[10rem] before:content-[''] before:bg-gradient-to-b before:from-dark",
+        "after:absolute after:bottom-0 after:left-0 after:w-full after:h-[10rem] after:content-[''] after:bg-gradient-to-t after:from-dark -z-[1]",
+      ])}
       ref={ref}
     >
       <img
@@ -44,7 +49,7 @@ const DontKnowYet = () => {
         alt="pattern"
         width={1000}
         height={800}
-        className="absolute top-0 left-0 w-full h-full object-cover -z-[1]"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-[2]"
       />
 
       <div className="container items-center h-full grid grid-cols-2 gap-10">
